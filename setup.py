f02dd9679bb42153694f48ff18f50f00cbf70e9e
Malik Hacini <juioiplm@gmail.com>
2024-06-12 18:19:04 +0200

    setup update

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='GSCpy',
     version='1.0.0',
-    description='Generalized spectral clustering in Python',
+    description='Generalized Spectral Clustering in Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Malik Hacini',
     author_email='mhacini.pro@gmail.com',
     url='https://github.com/Malik-Hacini/GSCpy',  # Update this with your project's URL
